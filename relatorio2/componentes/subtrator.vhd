25a51521193bfdd9dfc64c353abd32ce378539f4
gabsbsbsbs <gabrielsamoria1@gmail.com>
2023-09-28 10:48:33 -0300

    Adicionando os componentes da aula

---
+++ relatorio2/componentes/subtrator.vhd
@@ -0,0 +1,18 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+entity subtrator is
+	generic (n: integer := 8)
+	port (
+	add1, add2: in unsigned(n-1 downto 0);
+	sum : out unsigned(n downto 0)
+	);
+
+end subtrator;
+
+architecture arch of subtrator is
+begin
+	sum <= unsigned(abs(signed(resize(add1, n+1)) - signed(resize(add2, n+1))));
+
+end architecture;
